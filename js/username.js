306814b7eff6c9f94f5b43f860020dfaf70dcc85
renoirface <77360624+renoirface@users.noreply.github.com>
2021-01-12 12:55:49 -0800

    Update username.js

--- js/username.js
+++ js/username.js
@@ -1,4 +1,4 @@
 function username() {
-    return "MY_TWITCH_ID_HERE"; // <-- Fill in your username
+    return "CAELISALCREATIONS"; // <-- Fill in your username
                // example: return "ninja";
 }
